6ecb841b2edbe846d76f95b1b26e0cfe1fcb40f8
username <xinjiewang@XinjiedeMacBook-Pro.local>
2024-03-03 11:45:27 -0500

    4th commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,17 @@
 export default function Project () {
     return (
        
-        <h1>hi</h1>
+    
+        <div className="project"> 
+
+            <h2>  Mario World | Html|JavaScript|CSS</h2>
+            <h3> Mario World is a casual snake game. Using the arrow keys, the player moves Mario to collect mystery boxes while avoiding collisions with the game boundaries.
+                 Mario's growing trail of collected items, and the enemy, Bowser.
+            </h3>
+
+            <h2> CoolDog |  Html|JavaScript|CSS|Node.js|Ejs|MongDB  </h2>
+            <h3></h3>
+        </div>
         
      )
     }
